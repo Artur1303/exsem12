2bb169cf79609e4b72e414f1521175962c59dca7
artur <metis130383@gmail.com>
2021-01-23 12:46:35 +0600

    Реализовал главную страницу реализовал фото заглушку добавли ссылку на страницу пользователя добавленна  кнопка добавить  в  друзья

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -16,25 +16,6 @@ from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm
 from .models import AuthToken, Profile
 
 
-# def login_view(request):
-#     context = {}
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('webapp:index')
-#         else:
-#             context['has_error'] = True
-#     return render(request, 'registration/login.html', context=context)
-# 
-# 
-# def logout_view(request):
-#     logout(request)
-#     return redirect('webapp:index')
-
-
 class RegisterView(CreateView):
     model = User
     template_name = 'user_create.html'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    model = get_user_model()
+    paginate_comments_by = 2
+    paginate_comments_orphans = 0
+    context_object_name = 'users'
