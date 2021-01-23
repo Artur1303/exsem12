2b3ee9f9a72ce98d7f6745c9b05df9590b62de5b
artur <metis130383@gmail.com>
2021-01-23 19:17:46 +0600

    добавлена пагинация  главной страници

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -16,6 +16,8 @@ class IndexView(ListView):
     template_name = 'index.html'
     model = get_user_model()
     context_object_name = 'users'
+    paginate_by = 2
+    paginate_orphans = 0
 
     def get_queryset(self):
         my_user = super().get_queryset().exclude(pk=self.request.user.pk)
